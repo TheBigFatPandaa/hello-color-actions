334439dd1e573f59c69dcd260f3b707a4e97e588
Ankit Maheshwari <ankit.maheshwari@innovaccer.com>
2025-03-07 14:09:04 +0100

    modified hello page

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from the LH 762 FROM MUNICH TO DELHI !!";
 console.log(hello());
 exports.hello = hello;
